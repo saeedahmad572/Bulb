2c0ab844d61065f48613ed95ef875918cc3bf5a6
Saeed Ahmad Bloach <136958120+saeedahmad572@users.noreply.github.com>
2024-10-22 17:11:51 +0500

    Bulb

---
+++ bulb.js
@@ -0,0 +1,10 @@
+let bulbon = document.querySelector(".on");
+let bulboff = document.querySelector(".off");
+let msg = document.querySelector("#msg");
+let bulb = ()=>{
+if(bulbon){
+    msg.document.innerHTML = "Bulb is Off"; 
+}else{
+    msg.document.innerHTML = "Bulb is Off"; 
+}
+}
